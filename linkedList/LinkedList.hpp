c06d5f97868322ed3ad00a5aacfcadf4d0dcd0ea
Chris <chris@ariagno.com>
2020-02-03 15:17:23 -0600

    fixed the not great formatting of brackets

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -1,47 +1,41 @@
 /**
-*	@author 
-*	@date 
+*	@author
+*	@date
 *	@file LinkedList.hpp
 *	@brief Implementation file for templated LinkedList class
 */
 
 template <typename T>
-LinkedList<T>::LinkedList() : m_front(nullptr), m_size(0)
-{
+LinkedList<T>::LinkedList() : m_front(nullptr), m_size(0){
 
 }
 
 template <typename T>
-LinkedList<T>::~LinkedList() 
-{
-	while(!isEmpty())
-	{
+LinkedList<T>::~LinkedList() {
+	while(!isEmpty()) {
 		removeFront();
 	}
 }
 
 template <typename T>
-bool LinkedList<T>::isEmpty() const
-{
+bool LinkedList<T>::isEmpty() const {
 	return(m_size == 0);
 }
 
 template <typename T>
-int LinkedList<T>::size() const
-{
-	/** TODO 
+int LinkedList<T>::size() const {
+	/** TODO
 		Fix this method
 	*/
 	return(0);
 }
 
 template <typename T>
-bool LinkedList<T>::search(T value) const
-{
+bool LinkedList<T>::search(T value) const {
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO 
+	/** TODO
 		Fix this method
 	*/
 
@@ -49,19 +43,17 @@ bool LinkedList<T>::search(T value) const
 }
 
 template <typename T>
-std::vector<T> LinkedList<T>::toVector() const
-{
+std::vector<T> LinkedList<T>::toVector() const {
 	std::vector<T> vec;
 	Node<T>* temp = m_front;
 
-	while( temp != nullptr )
-	{
+	while( temp != nullptr ) {
 		vec.push_back(temp->getValue());
 		temp = temp->getNext();
 	}
 
-	return(vec);	
-	
+	return(vec);
+
 }
 
 template <typename T>
@@ -69,27 +61,22 @@ void LinkedList<T>::addBack(T value)
 {
 	Node<T>* temp = nullptr;
 
-	if(isEmpty())
-	{
-		m_front = new Node<T>(value);	
-	}
-	else
-	{
+	if(isEmpty()) {
+		m_front = new Node<T>(value);
+	} else {
 		temp = m_front;
-		while(temp->getNext() != nullptr)
-		{
+		while(temp->getNext() != nullptr) {
 			temp = temp->getNext();
 		}
 
-		temp->setNext( new Node<T>(value) );		
+		temp->setNext( new Node<T>(value) );
 	}
 
 	m_size++;
 }
 
 template <typename T>
-void LinkedList<T>::addFront(T value)
-{
+void LinkedList<T>::addFront(T value) {
 	Node<T>* temp = m_front;
 	m_front = new Node<T>(value);
 	m_front->setNext( temp );
@@ -97,27 +84,24 @@ void LinkedList<T>::addFront(T value)
 }
 
 template <typename T>
-bool LinkedList<T>::removeBack()
-{
+bool LinkedList<T>::removeBack() {
 	Node<T>* lastNode = nullptr;
 	Node<T>* secondintoLast = nullptr;
 	bool isRemoved = false;
 
-	/** TODO 
+	/** TODO
 		Fix this method
 	*/
 
 	return(isRemoved);
-}	
+}
 
 template <typename T>
-bool LinkedList<T>::removeFront()
-{
+bool LinkedList<T>::removeFront() {
 	Node<T>* temp = nullptr;
 	bool isRemoved = false;
 
-	if(!isEmpty())
-	{
+	if(!isEmpty()) {
 		temp = m_front;
 		m_front = temp->getNext();
 		delete temp;
